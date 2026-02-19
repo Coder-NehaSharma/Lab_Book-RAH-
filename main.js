5421495fefac2404fdfc5c2e7074531c5a8923b5
Coder-NehaSharma <neha1938sharma@gmail.com>
2026-02-19 13:42:57 +0530

    Config: Add Electron Builder and GitHub Actions workflow

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        icon: path.join(__dirname, 'icon.png'), // Placeholder, will default if missing
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+
+    win.loadFile('index.html');
+    win.removeMenu(); // Remove default menu for kiosk-like feel
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
